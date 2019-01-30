4a862632488c71026f9c404a911e7f49b1747872
Stephanie Gilley <stephanie.m.gilley@gmail.com>
2019-01-29 19:30:57 -0600

    removed unused imports

--- src/main/java/org/launchcode/amenti/Controllers/AmentiController.java
+++ src/main/java/org/launchcode/amenti/Controllers/AmentiController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 

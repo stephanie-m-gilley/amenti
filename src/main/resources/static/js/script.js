d7a162eeb6ecad5f69938a48c00eb9cec8a78355
Stephanie Gilley <stephanie.m.gilley@gmail.com>
2019-02-19 20:22:16 -0600

    cleaned out unnecessary js code

--- src/main/resources/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function openCity(evt, cityName) {
-    var i;
-    var x = document.getElementByClassName("city");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(city Name).style.display = "block";
-    }

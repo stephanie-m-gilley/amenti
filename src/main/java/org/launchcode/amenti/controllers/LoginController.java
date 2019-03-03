de14cfb4bfb7664a1b4871844eacd05a9778347f
Stephanie Gilley <stephanie.m.gilley@gmail.com>
2019-03-03 00:04:09 -0600

    added a register link to log in

--- src/main/java/org/launchcode/amenti/controllers/LoginController.java
+++ src/main/java/org/launchcode/amenti/controllers/LoginController.java
@@ -1,54 +1,27 @@
 package org.launchcode.amenti.controllers;
-
-
-import org.launchcode.amenti.models.User;
 import org.launchcode.amenti.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-@Controller
-@RequestMapping(value = "login")
-public class LoginController {
-
-     @Autowired
-     UserDao userDao;
+    @Autowired
+    UserDao userDao;
 
 
-        @RequestMapping(method = RequestMethod.GET)
-        public String index(Model model) {
-            model.addAttribute("title", "Amenti");
-            return "login/index";
-        }
 
-        @RequestMapping(value = "login", method = RequestMethod.POST)
-        public String login(
-                @RequestParam("username") String username,
-                @RequestParam("password") String password,
-                HttpSession session,
-                ModelMap modelMap) {
-            User user = userDao.findByUsernameAndPassword(username, password);
-            if(user!= null) {
-                session.setAttribute("user", user);
-                return "amenti/index";
-            } else {
-                modelMap.put("error", "Invalid Account");
-                return "redirect:/login";
-            }
-        }
-
-        @RequestMapping(value = "logout", method = RequestMethod.GET)
-        public String logout(HttpSession session) {
-            session.removeAttribute("username");
-            return "redirect:/login";
-        }
-
-    }
+}
 
